d15b6c7ed595b45cb3cdbf4e564554d860f9b667
Vinod Kumar <vinodkumar@192.168.1.3>
2025-05-18 12:28:29 +0530

    new file:   README-ch04.md 	modified:   pom.xml 	modified:   src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java 	new file:   src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java 	modified:   src/main/resources/application.yml

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,7 +1,13 @@
 package com.polarbookshop.catalogservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 
 /*
@@ -11,10 +17,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
+	@Autowired
+	private Environment environment;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogServiceApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(CatalogServiceApplication.class, args);
+		System.out.println(applicationContext.getBean(CatalogServiceApplication.class).environment.getProperty("server.port"));
 	}
 
+	@Bean
+	ApplicationRunner applicationRunner(Environment environment) {
+		return args -> {
+			System.out.println("applicationRunner: " + environment.getProperty("server.port"));
+			System.out.println("applicationRunner: " + environment.getProperty("polar.greeting"));
+		};
+	}
 }
